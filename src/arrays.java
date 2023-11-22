fa55e8d9a927623a1c671e58efc7382a9bbde600
UngureanuAdrianPaul <adrianpaul.s371152@cesurformacion.com>
2023-11-22 13:25:55 +0100

    array

---
+++ src/arrays.java
@@ -0,0 +1,17 @@
+
+public class arrays {
+
+	public static void main(String[] args) {
+
+		int[] listaInteger = new int[10];
+		
+		
+		for (int i=0; i<10; i++) {
+			listaInteger[i]=i;
+			
+			System.out.println(listaInteger[i]);
+		}
+
+	}
+
+}
