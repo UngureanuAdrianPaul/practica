8872088d406c0545d20f782725848a3c05c14716
UngureanuAdrianPaul <adrianpaul.s371152@cesurformacion.com>
2023-12-04 13:29:04 +0100

    objetos 1

---
+++ src/objetos.java
@@ -0,0 +1,64 @@
+
+public class objetos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String [] cadenaInicial = new String[10];
+		for (int i= 0; i<3; i++) {
+			cadenaInicial[i] = "hola";
+			
+		}
+		ListaFijaCadena cadena1 = new ListaFijaCadena(3,cadenaInicial);
+		System.out.println("num elementos: " + cadena1.getNumEle());
+		
+		for (int i=0; i< cadena1.getNumEle(); i++) {
+			System.out.println(cadena1.getArray()[i]);
+		}
+		
+		
+	}
+}
+
+
+class ListaFijaCadena {
+
+	private int numEle;
+	private String[] array = new String [MAXELE];
+	private static final int MAXELE = 10;
+
+	
+	public ListaFijaCadena(int numEle, String[] array) {
+		super();
+		this.numEle = numEle;
+		this.array = array;
+	}
+	
+	public int getNumEle() {
+		return numEle;
+	}
+
+	public void setNumEle(int numEle) {
+		this.numEle = numEle;
+	}
+
+	public String[] getArray() {
+		return array;
+	}
+
+	public void setArray(String[] array) {
+		this.array = array;
+	}
+
+	public static int getMaxele() {
+		return MAXELE;
+	}
+
+	
+
+	
+}
+
+
+
