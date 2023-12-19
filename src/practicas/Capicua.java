c81222c4542e93e4174daaefef2868fbb6c7aa68
UngureanuAdrianPaul <adrianpaul.s371152@cesurformacion.com>
2023-12-19 17:10:32 +0100

    capi 2

--- src/practicas/Capicua.java
+++ src/practicas/Capicua.java
@@ -8,11 +8,26 @@ public class Capicua {
 		Scanner introduce = new Scanner(System.in);
 		
 		int num = 0;
+		boolean correcto = false;
+		
 		
 		do {
 			System.out.print("Introduce un número de 3 cifras: ");
 			num = introduce.nextInt();
-		} while ((num / 1000 != 0) || ((num / 1000 == 0) && (num / 100 == 0) && (num / 10 == 0)));
+			if (num/1000==0) {
+				if(num/100 != 0) {
+					if(num/10 !=0) {
+						correcto = true;
+						
+					}
+					
+				}
+				
+			}
+		} while (!correcto);
+		
+		System.out.println("El número es de 3 cifras");
+		
 		introduce.close();
 		
 	}
