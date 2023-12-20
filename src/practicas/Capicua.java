abbd45b6a864ed8444463673502fa6bd4bea889a
UngureanuAdrianPaul <adrianpaul.s371152@cesurformacion.com>
2023-12-20 12:49:05 +0100

    version sin do while (generico)

--- src/practicas/Capicua.java
+++ src/practicas/Capicua.java
@@ -12,11 +12,11 @@ public class Capicua {
 		int cifra = 0;
 		int inv = 0;
 		int aux = 0;
-		do {
+		
 			System.out.print("Introduce un número de 3 cifras: ");
 			num = introduce.nextInt();
 
-		} while ((num / 1000 != 0) || (num / 100 == 0) || (num / 10 == 0));
+		
 
 		System.out.println("El número es de 3 cifras");
 		aux = num;
