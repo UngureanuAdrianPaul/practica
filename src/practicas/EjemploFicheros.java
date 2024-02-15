52d86dd96e056e2b5e3a54ec26d84654b0245e62
UngureanuAdrianPaul <adrianpaul.s371152@cesurformnacion.com>
2024-02-15 12:20:44 +0100

    Practicando con ficheros y flujos

---
+++ src/practicas/EjemploFicheros.java
@@ -0,0 +1,47 @@
+package practicas;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class EjemploFicheros {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream entrada = null;
+		FileOutputStream salida = null;
+
+		try {
+			entrada = new FileInputStream("pruebaFichero.txt");
+			int asci = entrada.read();
+			while (asci != -1) {
+				System.out.println((char) asci + " ");
+				asci = entrada.read();
+
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (entrada != null) {
+				try {
+					entrada.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		try {
+			salida = new FileOutputStream("pruebaFichero.txt");
+			salida.write((int) 'H');
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (salida != null) {
+				salida.close();
+			}
+		}
+	}
+}
---
+++ src/practicas/EjemploFicheros2.java
@@ -0,0 +1,66 @@
+package practicas;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class EjemploFicheros2 {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream entrada = null;
+		FileOutputStream salida = null;
+		Scanner teclado = new Scanner(System.in);
+
+		String frase;
+		char[] array;
+		
+		//INPUT
+		try {
+			entrada = new FileInputStream("pruebaFichero.txt");
+			int asci = entrada.read();
+			while (asci != -1) {
+				System.out.println((char) asci + " ");
+				asci = entrada.read();
+
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (entrada != null) {
+				try {
+					entrada.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		//OUTPUT 
+		try {
+			salida = new FileOutputStream("pruebaFichero.txt");
+			
+			System.out.println("Introuduce una frase: ");
+			frase = teclado.nextLine();
+			array = new char[frase.length()];
+
+			for (int i = 0; i < frase.length(); i++) {
+				salida.write((int) frase.charAt(i)) ;
+				array[i] = frase.charAt(i);
+				System.out.print(array[i]);
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (salida != null) {
+				salida.close();
+			}
+		}
+
+		teclado.close();
+	}
+}
