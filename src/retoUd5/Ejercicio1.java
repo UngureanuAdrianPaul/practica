3085fb81ae948cc0723ae3867f0732aec41b6420
UngureanuAdrianPaul <adrianpaul.s371152@cesurformnacion.com>
2024-03-07 12:26:00 +0100

    Reto Unidad 5 Adrian Paul

--- src/retoUd5/Ejercicio1.java
+++ src/retoUd5/Ejercicio1.java
@@ -5,12 +5,114 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-	} // fin MAIN
+		// INTRODUCIR NOMBRE DE FICHEROS
+
+		Scanner teclado = new Scanner(System.in);
+
+		String nombre1;
+		String nombre2;
+
+		do {
+			System.out.println("Introduce el nombre del primer fichero: ");
+			nombre1 = teclado.nextLine();
+
+		} while (nombre1.length() < 3);
+		System.out.println("El nombre del primer fichero " + nombre1 + " es correcto! \n");
+
+		do {
+			System.out.println("Introduce el nombre del segundo fichero: ");
+			nombre2 = teclado.nextLine();
+		} while (nombre2.length() < 3);
+		System.out.println("El nombre del segundo fichero " + nombre2 + " es correcto! \n");
+
+		// MOSTRAR POR PANTALLA EL DIRECTORIO ACTUAL DE TRABAJO
+
+		System.out.println(System.getProperty("user.dir") + "\n");
+
+		// RUTA RELATIVA 1
+
+		String directorioActual1 = System.getProperty("user.dir");
+		String rutaCompletaFich1 = directorioActual1 + File.separator + nombre1;
+		System.out.println(rutaCompletaFich1);
+		File fichero1 = new File(rutaCompletaFich1);
+
+		if (comprobarExiste(nombre1)) {
+			System.out.println("El fichero " + nombre1 + " ya existe.");
+		} else {
+			fichero1.createNewFile();
+		}
+
+		// RUTA RELATIVA 2
+
+		String directorioActual2 = System.getProperty("user.dir");
+		String rutaCompletaFich2 = directorioActual2 + File.separator + nombre2;
+		System.out.println(rutaCompletaFich2);
+		File fichero2 = new File(rutaCompletaFich2);
+
+		if (comprobarExiste(nombre2)) {
+			System.out.println("El fichero " + nombre2 + " ya existe.");
+		} else {
+			fichero2.createNewFile();
+		}
+
+		// ESCRIBIR EN FICHERO1
+
+		if (comprobarExiste(rutaCompletaFich1)) {
+			escribirEnFichero(fichero1);
+		}
+
+		// LEER FICHERO1
+
+		if (comprobarExiste(rutaCompletaFich1)) {
+			leerDeFichero(rutaCompletaFich1);
+		}
+
+		// SACAR DATOS POR PANTALLA
+
+		if (comprobarExiste(rutaCompletaFich1)) {
+			System.out.println(fichero1.getName());
+			System.out.println(fichero1.getAbsolutePath());
+			System.out.println(fichero1.getParent());
+			System.out.println(fichero1.length());
+			System.out.println("Es directorio? " + fichero1.isDirectory());
+			System.out.println();
+
+		} else {
+			System.out.println("No existe el fichero");
+		}
+
+		// DUPLICAR FICHEROS
+
+		duplicarFicheros(fichero1, fichero2);
+
+		// BORRAR FICHERO
+
+		borrarFichero(fichero1);
+
+		// MOSTRAR CONTENIDO FICHERO2
+
+		leerDeFichero(fichero2);
+
+		// CREAMOS DIRECTORIO NUEVO
+
+		File directorio = new File("dirEjer1");
+
+		if (comprobarExiste(directorio)) {
+			System.out.println("Ya existe el directorio. ");
+
+		} else {
+			directorio.mkdir();
+		}
+
+		teclado.close();
+
+	} // FINMAIN
 
 	// METODOS
 
@@ -22,7 +124,7 @@ public class Ejercicio1 {
 			entrada = new FileReader(miFichero);
 			int caracter = entrada.read();
 			while (caracter != -1) {
-				System.out.println((char) caracter);
+				System.out.print((char) caracter);
 				caracter = entrada.read();
 
 			}
@@ -46,7 +148,7 @@ public class Ejercicio1 {
 			int caracter = entrada.read();
 
 			while (caracter != -1) {
-				System.out.println((char) caracter);
+				System.out.print((char) caracter);
 				caracter = entrada.read();
 			}
 
@@ -69,7 +171,7 @@ public class Ejercicio1 {
 			salida = new FileWriter(miFichero);
 
 			for (int i = 0; i < 11; i++) {
-				salida.write((char) i);
+				salida.write("" + i);
 				salida.write('\n');
 			}
 
@@ -92,7 +194,7 @@ public class Ejercicio1 {
 			salida = new FileWriter(miFichero);
 
 			for (int i = 0; i < 11; i++) {
-				salida.write((char) i);
+				salida.write("" + i);
 				salida.write('\n');
 			}
 
@@ -124,30 +226,29 @@ public class Ejercicio1 {
 
 		return fichero.exists();
 	}
-	
-	static boolean comporbarExiste(String fichero) {
+
+	static boolean comprobarExiste(String fichero) {
 		File miFichero = new File(fichero);
-		
+
 		return miFichero.exists();
 	}
 
 	static boolean duplicarFicheros(File origen, File destino) throws IOException {
-		
+
 		FileReader entrada = null;
 		FileWriter salida = null;
-		
+
 		try {
 			entrada = new FileReader(origen);
 			salida = new FileWriter(destino);
-			
+
 			int caracter = entrada.read();
-			
+
 			while (caracter != -1) {
 				salida.write((char) caracter);
 				caracter = entrada.read();
 			}
-			
-			
+
 			return true;
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
@@ -159,13 +260,12 @@ public class Ejercicio1 {
 			if (entrada != null) {
 				entrada.close();
 			}
-			
+
 			if (salida != null) {
 				salida.close();
 			}
-		} 
-		
+		}
+
 	}
-	
-	
+
 } // fin CLASE
